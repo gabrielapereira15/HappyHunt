6883aac4992bc37f41ca72c3357a1070ed33d424
gabrielapersonal <gabrielanasoli15@gmail.com>
2024-04-13 17:10:46 -0400

    initial commit

---
+++ app/src/main/java/com/example/happyhunt/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.happyhunt;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.happyhunt.databinding.ActivityMainBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
+    ActionBarDrawerToggle mToggle;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private final static int REQUEST_CODE = 1;
+    Location currentLocation;
+    Intent intentMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+        init();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+    }
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null) {
+                    currentLocation = location;
+                }
+            }
+        });
+    }
+    private void askPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]
+                {android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==REQUEST_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Location permission is denied, please allow the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void init() {
+        mToggle = new ActionBarDrawerToggle(this, mainBinding.drawerLayout, mainBinding.materialToolbar, R.string.nav_open,R.string.nav_close);
+        mainBinding.drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        setSupportActionBar(mainBinding.materialToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        SetNavigationDrawer();
+        SetBottomNavigation();
+    }
+
+    private void SetNavigationDrawer() {
+        mainBinding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.nav_account_menu) {
+                    // Implement page
+                } else if(item.getItemId()==R.id.nav_about_menu) {
+                    // Implement page
+                } else if(item.getItemId()==R.id.nav_history_menu) {
+                    // Implement page
+                }
+                return false;
+            }
+        });
+    }
+
+    private void SetBottomNavigation() {
+        mainBinding.bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.nav_bottom_map) {
+                    intentMap = new Intent(MainActivity.this, MapActivity.class);
+                    startActivity(intentMap);
+                } else if(item.getItemId()==R.id.nav_bottom_filters) {
+                    // Implement page
+                } else if(item.getItemId()==R.id.nav_bottom_favorites) {
+                    // Implement page
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/happyhunt/MapActivity.java
@@ -0,0 +1,99 @@
+package com.example.happyhunt;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.happyhunt.databinding.ActivityMapBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap myMap;
+    ActivityMapBinding mapBinding;
+    ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mapBinding = ActivityMapBinding.inflate(getLayoutInflater());
+        View view = mapBinding.getRoot();
+        setContentView(view);
+        init();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        MarkerOptions options = new MarkerOptions().position(sydney).title("Sydney");
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        myMap.addMarker(options);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void init() {
+        mToggle = new ActionBarDrawerToggle(this, mapBinding.drawerLayout, mapBinding.materialToolbar, R.string.nav_open, R.string.nav_close);
+        mapBinding.drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        setSupportActionBar(mapBinding.materialToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        SetNavigationDrawer();
+        SetBottomNavigation();
+    }
+
+    private void SetNavigationDrawer() {
+        mapBinding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.nav_account_menu) {
+                    // Implement page
+                } else if(item.getItemId()==R.id.nav_about_menu) {
+                    // Implement page
+                } else if(item.getItemId()==R.id.nav_history_menu) {
+                    // Implement page
+                }
+                return false;
+            }
+        });
+    }
+
+    private void SetBottomNavigation() {
+        mapBinding.bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.nav_bottom_filters) {
+                    // Implement page
+                } else if(item.getItemId()==R.id.nav_bottom_favorites) {
+                    // Implement page
+                }
+                return false;
+            }
+        });
+    }
+}
